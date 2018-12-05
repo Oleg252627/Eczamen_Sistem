90037f212f9326c5ea5df0daf879094d46f52c92
Oleg <opletinenko@gmail.com>
2018-12-05 19:33:23 +0200

    finish

---
+++ Eczamen_sistem/Eczamen_sistem/Eczamen_sistem/ModelJson/Hotels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eczamen_sistem.ModelJson
+{
+    class Hotels
+    {
+        public int id { get; set; }
+        public string hotelName { get; set; }
+        public string cityName { get; set; }
+        public string countryName { get; set; }
+        public int stars { get; set; }
+        public int cost { get; set; }
+
+    }
+}
